564da1937ac3da16db6a10dbdc51c82f8029ea90
Muhzinmidland <muhzin1113@gmail.com>
2023-12-18 12:00:32 +0530

    media player

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Landingpage from './pages/Landingpage';
+import Watchhistory from './pages/Watchhistory';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Landingpage/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/history' element={<Watchhistory/>}/>
+      </Routes>
+      <Footer/>
+    </>
+  )
 }
 
 export default App;
